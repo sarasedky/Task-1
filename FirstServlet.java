ac91691f52aa7736c36566165fe0cb0156606eb8
sarasedky <sara.sedky2018@gmail.com>
2019-01-04 16:00:10 +0200

    Add files via upload

--- FirstServlet.java
+++ FirstServlet.java
@@ -22,8 +22,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class FirstServlet
  */
-@WebServlet(description = "My First Servlet", urlPatterns = { "/FirstServlet" , "/FirstServlet.do"},
-initParams = {@WebInitParam(name="id",value="1")})
+@WebServlet(description = "My First Servlet", urlPatterns = { "/FirstServlet" , "/FirstServlet.do"})
 public class FirstServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
     
